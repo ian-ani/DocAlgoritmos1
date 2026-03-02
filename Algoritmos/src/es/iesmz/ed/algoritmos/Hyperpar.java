65b5bcc9e243e4757b119ed3fba4d1350cb40960
Iris <irisaunav@gmail.com>
2026-03-02 09:06:26 +0100

    Crear clase Hyperpar

---
+++ Algoritmos/src/es/iesmz/ed/algoritmos/Hyperpar.java
@@ -0,0 +1,96 @@
+package es.iesmz.ed.algoritmos;
+
+/**
+ * Clase Hyperpar que comprueba si un número es Hyperpar o no.
+ * Para que un número sea Hyperpar, todos sus dígitos deben ser pares.
+ * @author Iris Auñón Navarro
+ */
+
+public class Hyperpar {
+    /* ATRIBUTOS */
+
+    private long numero;
+
+    /* CONSTRUCTOR */
+
+    /**
+     * Constructor para guardar el número a comprobar.
+     * @param numero long; este parámetro recibe el número a comprobar.
+     */
+    public Hyperpar(long numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * Método que devuelve el número del objeto.
+     * @return numero: long; devuelve el número a comprobar.
+     */
+
+    public long getNumero() {
+        return numero;
+    }
+
+    /**
+     * Método que cambia el valor del número.
+     * @param numero long; cambia el número actual por otro número.
+     */
+
+    public void setNumero(long numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * Método toString. Para poder mostrar el número guardado.
+     * @return String; muestra el número.
+     */
+
+    @Override
+    public String toString() {
+        return String.format("Número: %d", numero);
+    }
+
+    /* OTROS METODOS */
+
+    /**
+     * Método que comprueba si un número es Hyperpar o no. Dicho número es Hyperpar si todos sus dígitos son pares.
+     * @return boolean; devuelve true si todos los números son true, devuelve false en el primer número
+     * que no lo sea.
+     */
+
+    public boolean esHyperPar() {
+        // Declarar array de 'numeros' de tipo booleano teniendo como tamano el largo del numero (pasado a String)
+        boolean[] numeros = new boolean[String.valueOf(numero).length()];
+        // Variable usada como indice para el bucle while
+        int i = 0;
+
+        // Copia de 'numero'
+        long tmp = numero;
+
+        // Mientras 'tmp' sea mayor a 0
+        while (tmp > 0) {
+            // Se divide el numero entre 10 para discriminar la parte decimal
+            tmp = tmp / 10;
+
+            // Comprobar si es divisible entre 2 (par) o no, si lo es guardar el booleano en el array
+            if (tmp % 2 == 0) {
+                numeros[i] = true;
+            } else {
+                numeros[i] = false;
+            }
+
+            // Incrementar variable de cuenta
+            i++;
+        }
+
+        // Recorrer array de numeros con valores booleanos
+        for (int j = 0; j < numeros.length; j++) {
+            // En el primer momento que encuentra un numero impar, retorna 'false' porque ya no es hyperpar
+            if (!numeros[j]) {
+                return false;
+            }
+        }
+
+        // Si llega hasta aqui, es hyperpar
+        return true;
+    }
+}
