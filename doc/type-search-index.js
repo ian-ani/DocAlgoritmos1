98c403266c421a0c83db0cf5f7ba777d0f978010
Iris <irisaunav@gmail.com>
2026-03-02 09:07:13 +0100

    Añadir documentación inicial

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.iesmz.ed.algoritmos","l":"Hyperpar"},{"p":"es.iesmz.ed.algoritmos","l":"Prueba"}];updateSearchResults();
